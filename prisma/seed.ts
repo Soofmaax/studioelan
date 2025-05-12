967cf42991fae3b18462673a92324882d48279c3
Salwa <124178747+Soofmaax@users.noreply.github.com>
2025-05-12 21:04:24 +0200

    Updated seed.ts

---
+++ prisma/seed.ts
@@ -0,0 +1,8 @@
+{
+  "title": "Méditation",
+  "description": "Séances guidées pour apaiser l'esprit et développer la pleine conscience.",
+  "price": 18,
+  "duration": 45,
+  "level": Level.ALL_LEVELS,
+  "capacity": 20,
+}
