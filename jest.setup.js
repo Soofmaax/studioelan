9cb0280ff7abc71d6b5d10fb23065a87d2775e99
Salwa <124178747+Soofmaax@users.noreply.github.com>
2025-05-12 21:55:35 +0200

    Add course management tests

---
+++ jest.setup.js
@@ -0,0 +1,12 @@
+import '@testing-library/jest-dom';
+import 'whatwg-fetch';
+
+jest.mock('next/navigation', () => ({
+  useRouter() {
+    return {
+      push: jest.fn(),
+      replace: jest.fn(),
+      prefetch: jest.fn(),
+    };
+  },
+}));
